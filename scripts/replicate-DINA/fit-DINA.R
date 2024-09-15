c4d3db3782270d9bda3d0313607d1e0660ec405a
mengeks <garethalexuni@gmail.com>
2024-09-15 00:43:39 -0400

    Incorporate eta_type, baseline_type to DINA experiment script.
    Also created a test JSON for DINA experiment

--- scripts/replicate-DINA/fit-DINA.R
+++ scripts/replicate-DINA/fit-DINA.R
@@ -3,21 +3,39 @@ source("R/data-reader.R")
 source("R/non-time-varying-estimate.R")
 source("R/DINA-runner.R")
 
+# ## run a test
+# run_experiment(
+#   "scripts/replicate-DINA/config-DINA-test.json"
+# )
+
 # ## run the whole experiment
 # run_experiment(
 #   "scripts/replicate-DINA/config-DINA-n-200-R-200.json"
 # )
 
-## run the experiment of DINA, K = 2
-run_experiment(
-  "scripts/replicate-DINA/config-custom-cox-K2.json"
-)
-# about one minute
+# ## run the experiment of DINA, K = 2
+# run_experiment(
+#   "scripts/replicate-DINA/config-custom-cox-K2.json"
+# )
+# # about one minute
 # 
 # ## run the experiment of DINA, K = 8
 # run_experiment(
 #   "scripts/replicate-DINA/config-custom-cox-K8.json"
 # )
 # # about one minute
+
+
+
+## run the whole experiment for log, n-200
+run_experiment(
+  "scripts/replicate-DINA/config-DINA-log.json"
+)
+
+
+## run the whole experiment for log, n-500
+run_experiment(
+  "scripts/replicate-DINA/config-DINA-log-n-500.json"
+)
 
 
